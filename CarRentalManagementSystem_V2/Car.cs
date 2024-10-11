1c9821f887a51b9358b948fe03590c7e95f0884a
Kalaimakan <ut01482tic2024@gmail.com>
2024-10-11 16:50:49 +0530

    Q10

---
+++ CarRentalManagementSystem_V2/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementSystem_V2
+{
+    public class Car
+    {
+       string CarId { get; set; }
+       string Brand { get; set; }
+       string Model{ get; set; }
+       decimal RentalPrice { get; set; }
+
+    }
+}
