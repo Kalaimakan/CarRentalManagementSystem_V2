84c7e43a2c7b469d09c8c1eedd248d09b08bf1e7
Kalaimakan <ut01482tic2024@gmail.com>
2024-10-11 16:42:22 +0530

    Add project files.

---
+++ CarRentalManagementSystem_V2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRentalManagementSystem_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
